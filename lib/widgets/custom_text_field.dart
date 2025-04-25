6fd6f5b6fc98987b04c40af2ebc42914c55be09d
HaJaehyeong <hajae305@gmail.com>
2025-04-25 10:27:52 +0900

    modify custom text field

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -3,24 +3,44 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final bool obscureText;
-  final ValueChanged<String> onChanged;
+  final void Function(String)? onChanged;
 
   const CustomTextField({
     super.key,
     required this.hintText,
     this.obscureText = false,
-    required this.onChanged,
+    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       obscureText: obscureText,
-      decoration: InputDecoration(
-        hintText: hintText,
-        border: const OutlineInputBorder(),
-      ),
       onChanged: onChanged,
+      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        labelText: hintText,
+        border: const UnderlineInputBorder(),
+        labelStyle: const TextStyle(
+          fontWeight: FontWeight.w500,
+          color: Colors.grey,
+        ),
+        floatingLabelStyle: const TextStyle(
+          fontWeight: FontWeight.w500,
+          color: Colors.grey,
+        ),
+        isDense: true,
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        errorBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Color(0xFFee5d50)),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Colors.black54, width: 2),
+        ),
+      ),
     );
   }
 }
