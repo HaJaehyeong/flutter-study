4cd5435e865b5697b5d95339014754296c6d0c73
HaJaehyeong <hajae305@gmail.com>
2025-04-25 10:51:21 +0900

    modify signin screen design

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -13,51 +13,89 @@ class SigninScreen extends StatefulWidget {
 class _SigninScreenState extends State<SigninScreen> {
   final SigninViewModel viewModel = SigninViewModel();
 
+  Future<void> _handleLogin() async {
+    try {
+      await viewModel.login();
+
+      if (!mounted || !context.mounted) return;
+
+      Navigator.pushNamed(context, '/');
+    } catch (e) {
+      if (!mounted) return;
+      showDialog(
+        context: context,
+        builder:
+            (_) => AlertDialog(
+              title: const Text('로그인 실패'),
+              content: Text(e.toString()),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('확인'),
+                ),
+              ],
+            ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // NOTE(hajae): Scaffold -> Layout (전체 페이지 틀)
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
+      body: Container(
+        alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CustomTextField(hintText: 'ID', onChanged: viewModel.setId),
-            const SizedBox(height: 16),
-            CustomTextField(
-              hintText: 'Password',
-              obscureText: true,
-              onChanged: viewModel.setPassword,
-            ),
-            const SizedBox(height: 24),
-            PrimaryButton(
-              text: 'Sign In',
-              onPressed: () async {
-                try {
-                  await viewModel.login();
-
-                  if (!mounted) return;
-                  if (!context.mounted) return;
-
-                  Navigator.pushNamed(context, '/');
-                } catch (e) {
-                  if (!mounted) return;
-                  showDialog(
-                    context: context,
-                    builder:
-                        (_) => AlertDialog(
-                          title: const Text('로그인 실패'),
-                          content: Text(e.toString()),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.of(context).pop(),
-                              child: const Text('확인'),
-                            ),
-                          ],
-                        ),
-                  );
-                }
-              },
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Baro Factory',
+                  style: TextStyle(
+                    fontSize: 32,
+                    height: 1,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0,
+                  ),
+                ),
+                const Text(
+                  'Monitoring for Mobile',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                FractionallySizedBox(
+                  widthFactor: 0.6,
+                  child: CustomTextField(
+                    hintText: 'ID',
+                    onChanged: viewModel.setId,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                FractionallySizedBox(
+                  widthFactor: 0.6,
+                  child: CustomTextField(
+                    hintText: 'Password',
+                    obscureText: true,
+                    onChanged: viewModel.setPassword,
+                  ),
+                ),
+                const SizedBox(height: 48),
+                FractionallySizedBox(
+                  widthFactor: 0.6,
+                  child: PrimaryButton(
+                    text: 'Sign In',
+                    onPressed: _handleLogin,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
